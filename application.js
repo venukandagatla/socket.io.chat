05407c5e3c7725d68736749e1d430cc73df00f42
Venu Kandagatla <venu.kandagatla@gmail.com>
2015-06-23 18:01:18 +0530

    Added disconnect event

--- application.js
+++ application.js
@@ -6,8 +6,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//step 5 add disconnect event
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
